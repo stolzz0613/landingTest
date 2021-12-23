26816848c75af1944f8e35bc20ab7b7b5bdd55a3
AndresB <andres0613@utp.edu.co>
2021-12-23 15:48:05 -0500

    Updates

--- src/assets/index.js
+++
@@ -1,4 +0,0 @@
-export {default as background} from './background.jpg';
-export {default as mountains} from './mountains.png';
-export {default as face} from './face.png';
-export {default as vhs} from './vhs.png';
